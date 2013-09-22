e49dcedcd44336a6f734bf6eab3673da74b1401c
Eito Katagiri <eitoball@gmail.com>
2013-09-22 20:11:18 +0900

    support .on() to add event handler

--- vendor/assets/javascripts/jquery.textchange.js
+++ vendor/assets/javascripts/jquery.textchange.js
@@ -6,30 +6,38 @@
  * Released under the MIT License
  */
 (function ($) {
-	
+
 	$.event.special.textchange = {
-		
-		setup: function (data, namespaces) {
-		  $(this).data('lastValue', this.contentEditable === 'true' ? $(this).html() : $(this).val());
-			$(this).bind('keyup.textchange', $.event.special.textchange.handler);
-			$(this).bind('cut.textchange paste.textchange input.textchange', $.event.special.textchange.delayedHandler);
-		},
-		
-		teardown: function (namespaces) {
-			$(this).unbind('.textchange');
-		},
-		
+
+    add: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+		  elem.data('lastValue', elem[0].contentEditable === 'true' ? elem.html() : elem.val());
+			elem.bind('keyup.textchange', $.event.special.textchange.handler);
+			elem.bind('cut.textchange paste.textchange input.textchange', $.event.special.textchange.delayedHandler);
+    },
+
+    remove: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+			elem.unbind('.textchange');
+    },
+
 		handler: function (event) {
 			$.event.special.textchange.triggerIfChanged($(this));
 		},
-		
+
 		delayedHandler: function (event) {
 			var element = $(this);
 			setTimeout(function () {
 				$.event.special.textchange.triggerIfChanged(element);
 			}, 25);
 		},
-		
+
 		triggerIfChanged: function (element) {
 		  var current = element[0].contentEditable === 'true' ? element.html() : element.val();
 			if (current !== element.data('lastValue')) {
@@ -38,39 +46,55 @@
 			}
 		}
 	};
-	
+
 	$.event.special.hastext = {
-		
-		setup: function (data, namespaces) {
-			$(this).bind('textchange', $.event.special.hastext.handler);
+
+    add: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+			elem.bind('textchange', $.event.special.hastext.handler);
 		},
-		
-		teardown: function (namespaces) {
-			$(this).unbind('textchange', $.event.special.hastext.handler);
+
+    remove: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+			elem.unbind('textchange', $.event.special.hastext.handler);
 		},
-		
+
 		handler: function (event, lastValue) {
 			if ((lastValue === '') && lastValue !== $(this).val()) {
 				$(this).trigger('hastext');
 			}
 		}
 	};
-	
+
 	$.event.special.notext = {
-		
-		setup: function (data, namespaces) {
-			$(this).bind('textchange', $.event.special.notext.handler);
+
+    add: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+			elem.bind('textchange', $.event.special.notext.handler);
 		},
-		
-		teardown: function (namespaces) {
-			$(this).unbind('textchange', $.event.special.notext.handler);
+
+    remove: function (handleObj) {
+      var elem = $(this);
+      if (handleObj.selector != undefined) {
+        elem = elem.find(handleObj.selector);
+      }
+			elem.unbind('textchange', $.event.special.notext.handler);
 		},
-		
+
 		handler: function (event, lastValue) {
 			if ($(this).val() === '' && $(this).val() !== lastValue) {
 				$(this).trigger('notext');
 			}
 		}
-	};	
+	};
 
 })(jQuery);
